5ab93d642ef3f138c26bdeacf948227357ce808d
Subhadip Maji <subhadipmaji700@gmail.com>
2021-03-22 11:48:26 +0530

    Chatbot design in intial phase

--- frontend/src/components/customChatbot.jsx
+++ frontend/src/components/customChatbot.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ChatBot from "react-simple-chatbot";
 import { ThemeProvider } from "styled-components";
-function customChatbot(props) {  const config = {
-    width:"400px",
+function CustomChatbot(props) {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [q1, setQ1] = useState([]);
+  const steps = [{
+    id: "Greet",
+    message: "Hello there, this is Emilia. How may I help you?",
+    delay: 5,
+    trigger: "question",
+  },
+  {
+    id: "acknowledge",
+    message: "Thank you!!",
+    end: true
+  }];
+  
+  console.log(props);
+  useEffect(() => {
+    fetch(props.data)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result[0].subsections);
+          let st=0;
+          for (let i = 0; i < result[0].subsections.length; i = i + 1) {
+            q1.push({value:i+15321133, label: result[0].subsections[i].type, trigger: "qs"+st });
+            st = st + 1;
+          }
+        
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+      (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  steps.push({id:"question",options:q1});
+  for (let i = 0; i < items.length; i = i + 1) {
+    let ar=[];
+    
+   for(let j=0; j<items[i].questions.length;j=j+1)
+    {
+      ar.push({value:i*10+j,label:items[i].questions[j],trigger:"ans"+i+j});
+      
+    }
+   steps.push({id:"qs"+i,options:ar});
+   for(let j=0; j<items[i].questions.length;j=j+1)
+    {
+      steps.push({id:"ans"+i+j,message:items[i].answers[j],trigger:"Greet"});
+      
+    }
+   
+
+  }
+  console.log(steps.length);
+  const config = {
+    width: "400px",
     height: "400px",
-    floating: true
+    floating: true,
   };
   const theme = {
     background: "white",
@@ -15,53 +75,26 @@ function customChatbot(props) {  const config = {
     botBubbleColor: "#212529",
     botFontColor: "#fff",
     userBubbleColor: "#fff",
-    userFontColor: "#4c4c4c"
-   };
+    userFontColor: "#4c4c4c",
+  };
   
-  const steps = [
-     {
-      id: "Greet",
-      message: "Hello there, this is Emilia. How may I help you?",
-      delay: 5,
-      trigger: "info"
-     },
-     {
-      id: "info",
-      message: "Before we move ahead could you share your number with us so that we can be in contact with you??",
-      
-      trigger: '1'
-     },
-     {
-      id: '1',
-      user: true,
-      validator: (value) => {
-        if (isNaN(value)) {
-          return 'value should be a number';
-        }
-        return true;
-      },
-      trigger: 'last',
-    },
-    {
-      id:'last',
-      message: 'Thank you!!',
-      end: true
-    }
 
-   ];
-   const img="https://avatarfiles.alphacoders.com/547/thumb-54759.png";
-   
-   
-  return(
-    
+  console.log(steps);
+
+  
+  console.log(items);
+ 
+  const img = "https://avatarfiles.alphacoders.com/547/thumb-54759.png";
+
+  return (
     <ThemeProvider theme={theme}>
-      
-      
-    <ChatBot
-    speechSynthesis={{ enable: true, lang: 'en' }}
-    steps={steps} {...config} botAvatar={img}/>
-   
-        </ThemeProvider>
-    );
-   
-}export default customChatbot;
+      <ChatBot
+        //speechSynthesis={{ enable: true, lang: 'en' }}
+        steps={steps}
+        {...config}
+        botAvatar={img}
+      />
+    </ThemeProvider>
+  );
+}
+export default CustomChatbot;
