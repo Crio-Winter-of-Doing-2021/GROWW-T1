be4191778cf2e212d3b648a5b29b7d4a1d59215b
Subhadip Maji <subhadipmaji700@gmail.com>
2021-03-22 11:49:49 +0530

    design done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,49 @@
 import React from "react";
-import { BrowserRouter as Router,  Route, Switch } from "react-router-dom";
-import { Navigation, Stocks , MutualFunds, Deposits, Gold, UsStocks, Home} from "./components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, Page, Products } from "./components";
+
+
+
 
 function App() {
+  
   return (
+
     <div className="App">
-      
+
       <Router>
-      <Route path="/" exact component={() => < Home/>} />
-        <Navigation />
-        <Switch>
-          <Route path="/explore/stocks" exact component={() => < Stocks/>} />
-          <Route path="/stocks/:stock" exact component={() => < Stocks/>} />
-          <Route path="/explore/mutual-funds" exact component={() => <MutualFunds />} />
-          <Route path="/mutual-funds/:mutual-fund" exact component={() => <MutualFunds />} />
-          <Route path="/explore/deposits" exact component={() => <Deposits />} />
-          <Route path="/deposits/:deposit" exact component={() => <MutualFunds />} />
-          <Route path="/explore/gold" exact component={() => <Gold />} />
-         
-          <Route path="/explore/us-stocks" exact component={() => <UsStocks />} />
-          <Route path="/us-stocks/:us-stock" exact component={() => <Gold />} />
-          
-
-          
-
-        </Switch>
+        <Route path="/" exact component={() => < Home />} />
         
+            
+                <Switch>
+                  <Route path="/explore/stocks" exact component={() => < Page data="http://localhost:8080/stocks" header="Stocks" loc="/explore/stocks/" />} />
+                  <Route path="/stocks/:stock" exact component={() => < Products />} />
+                  <Route path="/explore/mutual-funds" exact component={() => <Page data="http://localhost:8080/mutual_funds" header="Mutual Funds" loc="/explore/mutual-funds/"/>} />
+                  <Route path="/mutual-funds/:mutual-fund" exact component={() => <Products />} />
+               
+                  <Route path="/explore/gold" exact component={() => <Page header="Gold"/>} />
+
+                  <Route path="/explore/us-stocks" exact component={() => <Page data="http://localhost:8080/us_stocks" header="US Stocks" loc="/explore/us-stocks/"/>} />
+                  <Route path="/us-stocks/:us-stock" exact component={() => <Products />} />
+
+                </Switch>
+
+    
+
+
+
+
+
+
+
+
+
         <footer />
       </Router>
       <Router>
-      
+
       </Router>
-      
+
     </div>
   );
 }
