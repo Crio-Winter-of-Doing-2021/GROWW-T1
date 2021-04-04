77c04ac7adcfceb3442c012ef0e736a307f51361
Subhadip Maji <subhadipmaji700@gmail.com>
2021-04-04 13:31:49 +0530

    fixed a bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,16 +17,16 @@ function App() {
             
                 <Switch>
                 <Route path="/" exact component={() => < Home />} />
-                  <Route path="/explore/stocks" exact component={() => < Page data="http://localhost:8080/stocks" steps="http://localhost:8080/faq_steps/stocks" header="Stocks" loc="/explore/stocks/" />} />
-                  <Route path="/explore/stocks/:id" exact component={() => < Products data="http://localhost:8080/stocks/" steps="http://localhost:8080/faq_steps/product/"/>} />
-                  <Route path="/explore/mutual-funds" exact component={() => <Page data="http://localhost:8080/mutual_funds"  steps="http://localhost:8080/faq_steps/mutual_funds" header="Mutual Funds" loc="/explore/mutual-funds/"/>} />
-                  <Route path="/explore/mutual-funds/:id" exact component={() => <Products data="http://localhost:8080/mutual_funds/" steps="http://localhost:8080/faq_steps/product/"/>} />
+                  <Route path="/explore/stocks" exact component={() => < Page data="http://localhost:8080/stocks" steps="http://localhost:8000/faq_steps/stocks" header="Stocks" loc="/explore/stocks/" />} />
+                  <Route path="/explore/stocks/:id" exact component={() => < Products data="http://localhost:8080/stocks/" steps="http://localhost:8000/faq_steps/product/"/>} />
+                  <Route path="/explore/mutual-funds" exact component={() => <Page data="http://localhost:8080/mutual_funds"  steps="http://localhost:8000/faq_steps/mutual_funds" header="Mutual Funds" loc="/explore/mutual-funds/"/>} />
+                  <Route path="/explore/mutual-funds/:id" exact component={() => <Products data="http://localhost:8080/mutual_funds/" steps="http://localhost:8000/faq_steps/product/"/>} />
                
-                  <Route path="/explore/gold" exact component={() => <Gold data="http://localhost:8080/gold"  steps="http://localhost:8080/faq_steps/gold" header="Digital Gold"/>} />
+                  <Route path="/explore/gold" exact component={() => <Gold data="http://localhost:8080/gold"  steps="http://localhost:8000/faq_steps/gold" header="Digital Gold"/>} />
 
-                  <Route path="/explore/us-stocks" exact component={() => <Page data="http://localhost:8080/us_stocks"  steps="http://localhost:8080/faq_steps/us_stocks" header="US Stocks" loc="/explore/us-stocks/"/>} />
-                  <Route path="/explore/us-stocks/:id" exact component={() => <Products data="http://localhost:8080/us_stocks/"  steps="http://localhost:8080/faq_steps/product/"/>} />
-                  <Route path="/orders" exact component={() => <Orders data="http://localhost:8080/orders/"  steps="http://localhost:8080/faq_steps/orders"/>} />
+                  <Route path="/explore/us-stocks" exact component={() => <Page data="http://localhost:8080/us_stocks"  steps="http://localhost:8000/faq_steps/us_stocks" header="US Stocks" loc="/explore/us-stocks/"/>} />
+                  <Route path="/explore/us-stocks/:id" exact component={() => <Products data="http://localhost:8080/us_stocks/"  steps="http://localhost:8000/faq_steps/product/"/>} />
+                  <Route path="/orders" exact component={() => <Orders data="http://localhost:8080/orders/"  steps="http://localhost:8000/faq_steps/orders"/>} />
                   <Route path="/profile" exact component={() => <Profile data="http://localhost:8080/profile/"  />} />
                   <Route path="/admin" exact component={() =><Redirect to ="/admin/pages" ></Redirect>} />
                   <Route path="/admin/pages" exact component={() => <AdminPage data="http://localhost:8080/"  />} />
